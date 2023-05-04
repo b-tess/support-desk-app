508099bd65a55413a2b03332ebc792a2da799d76
LogSyn <barbaratessn@gmail.com>
2023-05-04 16:49:55 +0300

    Production setup

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
     //Set build folder as static
     app.use(e.static(path.join(__dirname, '../frontend/build')))
     app.get('*', (req, res) => {
-        res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+        res.sendFile(__dirname, '../frontend/build/index.html')
     })
 } else {
     app.use('/', homeRouter)
